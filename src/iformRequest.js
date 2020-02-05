e7476af29d4ac571833fb16a5a8d80510e6146e7
tphamZerion <tpham@zerionsoftware.com>
2020-02-05 12:12:43 -0500

    adding api-key

--- src/iformRequest.js
+++ src/iformRequest.js
@@ -50,12 +50,15 @@ const _isAuthenticated  = () => {
 
 const _sendAPI =  (path, method, body)=>{
     let token = _getLocal(iformConfig.ACCESSTOKEN_KEY);
+    let apiKey = _getLocal(iformConfig.API_KEY);
     let options = { 
             method: method, 
             headers: {'Content-Type': 'application/json' }
         };
     if(body) options.body = JSON.stringify(body);
     if(token) options.headers.Authorization = `Bearer ${token}`;
+    if(apiKey) options.headers['x-api-key'] = apiKey;
+
     return new Promise((resolve, reject)=>{
         fetch(iformConfig.endpoint + path, options)
         .then(res=> {
@@ -65,12 +68,11 @@ const _sendAPI =  (path, method, body)=>{
         .catch(err=>{
             let status = err.status;
             if(status ==401) {
-                _removeLocal(ACCESSTOKEN_KEY);
-                _removeLocal(ACCESSTOKEN_EXP_KEY);
+                _removeLocal(iformConfig.ACCESSTOKEN_KEY);
+                _removeLocal(iformConfig.ACCESSTOKEN_EXP_KEY);
+                window.location.href = _getLocal(iformConfig.REDIRECT_KEY);
             }
-            if(err.hasOwnProperty("json"))
-                return err.json().then(data=>reject(data));
-            return reject(err);
+            err.json().then(data=>reject(data.error));
         })
     })
 }
@@ -100,13 +102,15 @@ const IformRequest  = {
     delete: (path, body)=> _sendInternal({path: `/${_getLocal(iformConfig.SERVERNAME_KEY)}/api${(path[0]!="/"?"/":"")}${path}`, method: 'delete', body: body}),
 
     configure: (config)=>{
-        let {servername, redirect_uri} = config;
+        let {servername, redirect_uri, apiKey} = config;
         if(!servername) throw new Error('Property "servername" is required');    
+        if(!apiKey) throw new Error('Property "apiKey" is required');    
         if(!redirect_uri) redirect_uri = window.location.href.split("?")[0];
         config.redirect_uri = redirect_uri;
         _setLocal(iformConfig.SERVERNAME_KEY, servername);
         _setLocal(iformConfig.REDIRECT_KEY, redirect_uri);
+        _setLocal(iformConfig.API_KEY, apiKey);
     }
 }
 
-export {IformRequest}
+export { IformRequest }
